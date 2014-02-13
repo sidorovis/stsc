3517d4c419e9967a6ffd3517fbf3f59f888c505e
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-02-13 12:04:30 +0300

    deleting unused imports

--- src/test/java/stsc/yahoofetcher/DownloadThreadTest.java
+++ src/test/java/stsc/yahoofetcher/DownloadThreadTest.java
@@ -1,12 +1,7 @@
 package stsc.yahoofetcher;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInput;
-import java.io.ObjectInputStream;
 import java.util.Calendar;
 import java.util.Date;
 
