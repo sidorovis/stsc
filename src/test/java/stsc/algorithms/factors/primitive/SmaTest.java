313a444422e417af63bbc570b65923579a3ec188
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-03-13 07:42:31 +0100

    adding SmaTest

---
+++ src/test/java/stsc/algorithms/factors/primitive/SmaTest.java
@@ -0,0 +1,46 @@
+package stsc.algorithms.factors.primitive;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.joda.time.LocalDate;
+
+import stsc.common.Day;
+import stsc.common.Stock;
+import stsc.common.UnitedFormatStock;
+import stsc.storage.BadSignalException;
+import stsc.storage.SignalsStorage;
+import junit.framework.TestCase;
+
+public class SmaTest extends TestCase {
+	public void testSma() throws IOException, BadSignalException {
+
+		final SignalsStorage signalsStorage = new SignalsStorage();
+
+		final Sma sma = new Sma();
+		sma.setExecutionName("testSma");
+		sma.setSignalsStorage(signalsStorage);
+
+		final Stock aapl = UnitedFormatStock.readFromUniteFormatFile("./test_data/aapl.uf");
+		final int aaplIndex = aapl.findDayIndex(new LocalDate(2013, 9, 4).toDate());
+		final ArrayList<Day> days = aapl.getDays();
+
+		for (int i = aaplIndex; i < days.size(); ++i) {
+			final Day day = days.get(i);
+			sma.process("aapl", day);
+		}
+
+		assertNull(signalsStorage.getStockSignal("testSma", days.get(aaplIndex).getDate()));
+		assertNull(signalsStorage.getStockSignal("testSma", days.get(aaplIndex + 2).getDate()));
+		assertNotNull(signalsStorage.getStockSignal("testSma", days.get(aaplIndex + 4).getDate()));
+		assertNotNull(signalsStorage.getStockSignal("testSma", days.get(days.size() - 1).getDate()));
+
+		Double lastSum = 0.0;
+		for (int i = days.size() - 5; i < days.size(); ++i) {
+			lastSum += days.get(i).getPrices().getOpen();
+		}
+		Day lastDay = days.get(days.size() - 1);
+		final double lastSma = ((Sma.Signal) signalsStorage.getStockSignal("testSma", lastDay.getDate())).value;
+		assertEquals(lastSum / 5, lastSma, 0.000001);
+	}
+}
