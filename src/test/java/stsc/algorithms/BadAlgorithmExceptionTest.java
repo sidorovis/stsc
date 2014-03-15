716be5ed31428e3da952a334f1ca5d821613050a
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-03-16 00:28:15 +0100

    adding BadAlgorithmException and EodAlgorithm tests

---
+++ src/test/java/stsc/algorithms/BadAlgorithmExceptionTest.java
@@ -0,0 +1,22 @@
+package stsc.algorithms;
+
+import junit.framework.TestCase;
+
+public class BadAlgorithmExceptionTest extends TestCase {
+	public void testBadAlgorithmException() {
+		BadAlgorithmException exception = new BadAlgorithmException("my exception message");
+		assertEquals("my exception message", exception.getMessage());
+		boolean bae = false;
+		boolean e = false;
+		try {
+			throw new BadAlgorithmException("the reason");
+		} catch (BadAlgorithmException ex) {
+			bae = true;
+			assertEquals("the reason", ex.getMessage());
+		} catch (Exception ex) {
+			e = true;
+		}
+		assertTrue(bae);
+		assertFalse(e);
+	}
+}
---
+++ src/test/java/stsc/algorithms/EodAlgorithmTest.java
@@ -0,0 +1,44 @@
+package stsc.algorithms;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import stsc.common.Day;
+import stsc.signals.EodSignal;
+import stsc.storage.BadSignalException;
+import stsc.storage.SignalsStorage;
+import stsc.storage.ThreadSafeStockStorage;
+import stsc.trading.Broker;
+import junit.framework.TestCase;
+
+public class EodAlgorithmTest extends TestCase {
+
+	private static class EodAlgorithmHelper extends EodAlgorithm {
+
+		protected EodAlgorithmHelper(String executionName, Broker broker, SignalsStorage signalsStorage,
+				AlgorithmSettings algorithmSettings) {
+			super(executionName, broker, signalsStorage, algorithmSettings);
+		}
+
+		@Override
+		public Class<? extends EodSignal> registerSignalsClass() {
+			return EodSignal.class;
+		}
+
+		@Override
+		public void process(Date date, HashMap<String, Day> datafeed) throws BadSignalException {
+			addSignal(date, new EodSignal());
+		}
+
+	}
+
+	public void testEodAlgorithm() throws BadSignalException {
+		final SignalsStorage ss = new SignalsStorage();
+		final Broker b = new Broker(new ThreadSafeStockStorage());
+		final AlgorithmSettings settings = new AlgorithmSettings();
+		EodAlgorithmHelper eah = new EodAlgorithmHelper("a", b, ss, settings);
+		final Date theDate = new Date();
+		eah.process(new Date(), new HashMap<String, Day>());
+		assertEquals(EodSignal.class, ss.getEodSignal("a", theDate).getSignal(EodSignal.class).getClass());
+	}
+}
