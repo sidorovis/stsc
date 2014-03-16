f8ffc3a5f05ce18edc53dcb86b958f3f78fb7e33
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-03-16 12:20:53 +0100

    adding TestingEodAlgorithmTest.java

---
+++ src/test/java/stsc/algorithms/privitive/TestingEodAlgorithmTest.java
@@ -0,0 +1,21 @@
+package stsc.algorithms.privitive;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import stsc.algorithms.AlgorithmSettings;
+import stsc.algorithms.primitive.TestingEodAlgorithm;
+import stsc.common.Day;
+import stsc.storage.BadSignalException;
+import stsc.storage.SignalsStorage;
+import stsc.storage.ThreadSafeStockStorage;
+import stsc.trading.Broker;
+import junit.framework.TestCase;
+
+public class TestingEodAlgorithmTest extends TestCase {
+	public void testTestingEodAlgorithm() throws BadSignalException {
+		TestingEodAlgorithm tea = new TestingEodAlgorithm("a", new Broker(new ThreadSafeStockStorage()),
+				new SignalsStorage(), new AlgorithmSettings());
+		tea.process(new Date(), new HashMap<String, Day>());
+	}
+}
