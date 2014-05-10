f638c29c5b23400fe64b80c3bca2515c30513d65
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-05-10 21:04:30 +0200

    add PerformanceCalculator

---
+++ src/main/java/stsc/performance/PerformanceCalculator.java
@@ -0,0 +1,96 @@
+package stsc.performance;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import stsc.algorithms.BadAlgorithmException;
+import stsc.signals.BadSignalException;
+import stsc.simulator.multistarter.StrategySearcher;
+import stsc.simulator.multistarter.StrategySearcherException;
+import stsc.simulator.multistarter.grid.MtStrategyGridSearcher;
+import stsc.simulator.multistarter.grid.SimulatorSettingsGridIterator;
+import stsc.statistic.StatisticsCalculationException;
+import stsc.statistic.StatisticsInnerProductFunction;
+import stsc.statistic.StatisticsSelector;
+import stsc.storage.StockStorage;
+import stsc.testhelper.TestHelper;
+import stsc.testhelper.TimeSearcher;
+
+public class PerformanceCalculator {
+
+	final private StockStorage stockStorage = TestHelper.getStockStorage();
+
+	private static int storedStrategyAmount = 500;
+
+	private static int threadsFrom = 1;
+	private static int threadsTo = 4;
+	private static int threadsStep = 2;
+
+	private List<String> periods = Arrays.asList(new String[] { "31-01-2000", "01-01-2001" });
+
+	// private List<String> periods = Arrays.asList(new String[] { "31-01-2000",
+	// "31-02-2000", "31-03-2000", "31-04-2000",
+	// "31-05-2000", "31-06-2000", "01-01-2001", "01-01-2002", "01-01-2003",
+	// "01-01-2004", "01-01-2005",
+	// "01-01-2006", "01-01-2007", "01-01-2008", "01-01-2009" });
+
+	private static int calculationsForAverage = 4;
+
+	static private class PerformanceStatistic {
+		public PerformanceStatistic(int threads, String period, double avTime) {
+			super();
+			this.threads = threads;
+			this.period = period;
+			this.avTime = avTime;
+		}
+
+		final public int threads;
+		final public String period;
+		final public double avTime;
+	}
+
+	private List<PerformanceStatistic> statistics = new ArrayList<>();
+
+	public PerformanceCalculator() throws Exception {
+		getTimeFor(1, "31-01-2000");
+		for (int threads = threadsFrom; threads <= threadsTo; threads += threadsStep) {
+			for (String endOfPeriod : periods) {
+				final double avTime = getTimeFor(threads, endOfPeriod);
+				statistics.add(new PerformanceStatistic(threads, endOfPeriod, avTime));
+			}
+		}
+	}
+
+	private long getTimeFor(int threads, String endOfPeriod) throws Exception {
+		final SimulatorSettingsGridIterator iterator = TestHelper.getSimulatorSettingsGridIterator(stockStorage,
+				Arrays.asList(new String[] { "open", "close", "high", "low" }), endOfPeriod);
+		final StatisticsSelector<Double> selector = new StatisticsSelector<Double>(storedStrategyAmount,
+				new StatisticsInnerProductFunction());
+		final MtStrategyGridSearcher searcher = new MtStrategyGridSearcher(iterator, selector, threads);
+		return calculateAverageTime(searcher);
+	}
+
+	private long timeForSearch(final StrategySearcher<Double> searcher) throws StrategySearcherException,
+			BadAlgorithmException, StatisticsCalculationException, BadSignalException {
+		final TimeSearcher timeSearcher = new TimeSearcher();
+		searcher.getSelector().getSelect();
+		return timeSearcher.finish();
+	}
+
+	private long calculateAverageTime(StrategySearcher<Double> searcher) throws Exception {
+		final int n = calculationsForAverage;
+		double av = 0.0;
+
+		for (int i = 0; i < n; ++i) {
+			av += timeForSearch(searcher);
+		}
+		return Math.round(av / calculationsForAverage);
+	}
+
+	public void printStdOut() {
+		for (PerformanceStatistic ps : statistics) {
+			System.out.println(ps.threads + "\t" + ps.period + "\t" + ps.avTime);
+		}
+	}
+}
--- src/main/java/stsc/performance/StrategySearcherPerformance.java
+++ src/main/java/stsc/performance/StrategySearcherPerformance.java
@@ -1,86 +1,11 @@
 package stsc.performance;
 
-import java.util.Arrays;
-
-import stsc.algorithms.BadAlgorithmException;
-import stsc.signals.BadSignalException;
-import stsc.simulator.multistarter.StrategySearcher;
-import stsc.simulator.multistarter.StrategySearcherException;
-import stsc.simulator.multistarter.grid.MtStrategyGridSearcher;
-import stsc.simulator.multistarter.grid.SimulatorSettingsGridIterator;
-import stsc.simulator.multistarter.grid.StrategyGridSearcher;
-import stsc.statistic.StatisticsCalculationException;
-import stsc.statistic.StatisticsInnerProductFunction;
-import stsc.statistic.StatisticsSelector;
-import stsc.storage.StockStorage;
-import stsc.testhelper.TestHelper;
-import stsc.testhelper.TimeSearcher;
-
 public class StrategySearcherPerformance {
-
-	private static int avSize = 4;
-	private static int storedStrategyAmount = 500;
-	private static int threadSize = 4;
-
-	private long timeForSearch(final StrategySearcher<Double> searcher) throws StrategySearcherException,
-			BadAlgorithmException, StatisticsCalculationException, BadSignalException {
-		final TimeSearcher timeSearcher = new TimeSearcher();
-		searcher.getSelector().getSelect();
-		return timeSearcher.finish();
-	}
-
-	private long calculateAverageTime(StrategySearcher<Double> searcher) throws Exception {
-		final int n = avSize;
-		double av = 0.0;
-
-		for (int i = 0; i < n; ++i) {
-			av += timeForSearch(searcher);
-		}
-		return Math.round(av / n);
-	}
-
-	private long testAvTime(StockStorage stockStorage, String finishPeriod) throws Exception {
-		final SimulatorSettingsGridIterator iterator = TestHelper.getSimulatorSettingsGridIterator(stockStorage,
-				Arrays.asList(new String[] { "open", "close", "high", "low" }), finishPeriod);
-		final StatisticsSelector<Double> selector = new StatisticsSelector<Double>(storedStrategyAmount,
-				new StatisticsInnerProductFunction());
-		final StrategyGridSearcher searcher = new StrategyGridSearcher(iterator, selector);
-		return calculateAverageTime(searcher);
-	}
-
-	private long testAvMtTime(StockStorage stockStorage, String finishPeriod) throws Exception {
-		final SimulatorSettingsGridIterator iterator = TestHelper.getSimulatorSettingsGridIterator(stockStorage,
-				Arrays.asList(new String[] { "open", "close", "high", "low" }), finishPeriod);
-		final StatisticsSelector<Double> selector = new StatisticsSelector<Double>(storedStrategyAmount,
-				new StatisticsInnerProductFunction());
-		final MtStrategyGridSearcher searcher = new MtStrategyGridSearcher(iterator, selector, threadSize);
-		return calculateAverageTime(searcher);
-	}
-
-	public StrategySearcherPerformance() throws Exception {
-
-		final StockStorage stockStorage = TestHelper.getStockStorage();
-		System.out.println(testAvTime(stockStorage, "31-01-2000"));
-		System.out.println(testAvMtTime(stockStorage, "31-01-2000"));
-		System.out.println(testAvTime(stockStorage, "31-01-2000"));
-		System.out.println(testAvMtTime(stockStorage, "31-01-2000"));
-		for (int i = 3; i <= 12; i += 3) {
-			final long testAvTime = testAvTime(stockStorage, "31-" + String.format("%02d", i) + "-2000");
-			final long testAvMtTime = testAvMtTime(stockStorage, "31-" + String.format("%02d", i) + "-2000");
-			System.out.println(i + " : " + testAvTime + " " + testAvMtTime);
-		}
-		avSize = 4;
-		for (int i = 2001; i <= 2001; i += 2) {
-			final long testAvTime = testAvTime(stockStorage, "31-01-" + String.format("%04d", i));
-			System.out.println(i + " 1t: " + testAvTime + " ");
-			final long testAvMtTime = testAvMtTime(stockStorage, "31-01-" + String.format("%04d", i));
-			System.out.println(i + " mt: " + testAvMtTime);
-		}
-	}
 
 	public static void main(String[] args) {
 		try {
-			new StrategySearcherPerformance();
+			final PerformanceCalculator ps = new PerformanceCalculator();
+			ps.printStdOut();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
