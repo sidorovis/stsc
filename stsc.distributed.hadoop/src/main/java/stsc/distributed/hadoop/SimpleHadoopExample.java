0dd26822f8037fe63ddd7cbef9034a876bfc605b
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-08-17 19:25:36 +0200

    #39 set distributed hadoop

---
+++ stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/SimpleHadoopExample.java
@@ -0,0 +1,20 @@
+package stsc.distributed.hadoop;
+
+import cascading.pipe.SubAssembly;
+
+class SimpleHadoopExample {
+	
+	private static class MapTest extends SubAssembly {
+		public MapTest(String name) {
+			
+		}
+	}
+	
+	public SimpleHadoopExample() {
+		
+	}
+
+	public static void main(String[] args) {
+		new SimpleHadoopExample();
+	}
+}
