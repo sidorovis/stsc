6335fe99ea21e4563d0ad642a6ac0e887d9f1790
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-08-23 17:09:56 +0200

    #39 add non targetet solution with map-reduce-reduce

---
+++ stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/MyFilter.java
@@ -0,0 +1,18 @@
+package stsc.distributed.hadoop;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MyFilter extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+	@Override
+	protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+		LongWritable lw = values.iterator().next();
+		if (lw.get() > 2) {
+			context.write(key, lw);
+		}
+	}
+}
---
+++ stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/MyReducer.java
@@ -0,0 +1,19 @@
+package stsc.distributed.hadoop;
+
+import java.io.IOException;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+	@Override
+	protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+		long sum = 0;
+		for (LongWritable v : values) {
+			sum += v.get();
+		}
+		context.write(key, new LongWritable(sum));
+	}
+
+}
