f885ceab88c74358d46dd37291c2f9093412a6e0
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-08-23 17:09:49 +0200

    #39 add non targetet solution with map-reduce-reduce

--- stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/MapReduceExample.java
+++ stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/MapReduceExample.java
@@ -1,32 +1,65 @@
 package stsc.distributed.hadoop;
 
+import java.io.IOException;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.MapFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class MapReduceExample extends Configured implements Tool {
 
+	@SuppressWarnings("deprecation")
 	@Override
-	public int run(String[] args) throws Exception {
-		Job job = new Job(new JobConf(this.getConf()), "word_count");
+	public int run(String[] args) {
+		Job job;
+		try {
+			job = new Job(new JobConf(this.getConf()), "word_count");
 
-		job.setJarByClass(MapReduceExample.class);
-		job.setMapperClass(MyMapper.class);
+			job.setJarByClass(MapReduceExample.class);
 
-		FileInputFormat.setInputPaths(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+			job.setMapperClass(MyMapper.class);
+			job.setCombinerClass(MyReducer.class);
+			job.setReducerClass(MyFilter.class);
+			
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(LongWritable.class);
+			
+			job.setInputFormatClass(TextInputFormat.class);
+			job.setOutputFormatClass(TextOutputFormat.class);
 
-		job.waitForCompletion(true);
+			FileInputFormat.setInputPaths(job, new Path(args[0]));
+			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+			job.waitForCompletion(true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return 0;
 	}
 
-	public static void main(String[] args) throws Exception {
-		args = new String[] { "in.txt", "out.txt" };
-		ToolRunner.run(new MapReduceExample(), args);
+	public static void main(String[] args) throws IOException {
+		// if (new File("out").exists()) {
+		// FileUtils.deleteDirectory(new File("out"));
+		// }
+		args = new String[] { "in.txt", "out" };
+		try {
+			ToolRunner.run(new MapReduceExample(), args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/MyMapper.java
+++ stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/MyMapper.java
@@ -4,14 +4,16 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MyMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+public class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+	private final static LongWritable one = new LongWritable(1);
 
 	public MyMapper() {
-
 	}
 
-	protected void map(LongWritable key, Text value, Context context) throws java.io.IOException, InterruptedException {
-		context.getCounter("mygroup", "jeff").increment(1);
-		context.write(key, value);
+	@Override
+	protected void map(LongWritable key, Text value, org.apache.hadoop.mapreduce.Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+			throws java.io.IOException, InterruptedException {
+		context.write(value, one);
 	};
 }
