a7864821dcb611a0b1fd1061d80b759e5f5363a1
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-09-01 22:30:42 +0200

    #40 add several additional checks SimulatorSettingsGeneticListWritableTest

--- stsc.distributed.hadoop/src/test/java/stsc/distributed/hadoop/types/SimulatorSettingsGeneticListWritableTest.java
+++ stsc.distributed.hadoop/src/test/java/stsc/distributed/hadoop/types/SimulatorSettingsGeneticListWritableTest.java
@@ -1,18 +1,21 @@
 package stsc.distributed.hadoop.types;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.hadoop.io.DataInputByteBuffer;
 import org.apache.hadoop.io.DataOutputByteBuffer;
 
 import stsc.common.algorithms.BadAlgorithmException;
 import stsc.general.simulator.multistarter.BadParameterException;
+import stsc.general.simulator.multistarter.genetic.GeneticExecutionInitializer;
 import stsc.general.simulator.multistarter.genetic.SimulatorSettingsGeneticList;
 import stsc.general.testhelper.TestGeneticSimulatorSettings;
 import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class SimulatorSettingsGeneticListWritableTest extends TestCase {
+
 	public void testSimulatorSettingsGeneticListWritable() throws BadAlgorithmException, IOException, BadParameterException {
 		final SimulatorSettingsGeneticList list = TestGeneticSimulatorSettings.getGeneticList();
 
@@ -29,7 +32,15 @@ public class SimulatorSettingsGeneticListWritableTest extends TestCase {
 
 		final SimulatorSettingsGeneticList listCopy = ssglCopy.getGeneticList(list.getStockStorage());
 
-		Assert.assertEquals(list.getStockInitializers().size(), listCopy.getStockInitializers().size());
-		Assert.assertEquals(list.getEodInitializers().size(), listCopy.getEodInitializers().size());
+		final List<GeneticExecutionInitializer> stocks = list.getStockInitializers();
+		final List<GeneticExecutionInitializer> stocksCopy = listCopy.getStockInitializers();
+		final List<GeneticExecutionInitializer> eods = list.getEodInitializers();
+		final List<GeneticExecutionInitializer> eodsCopy = listCopy.getEodInitializers();
+
+		Assert.assertEquals(stocks.size(), stocksCopy.size());
+		Assert.assertEquals(eods.size(), eodsCopy.size());
+
+		Assert.assertEquals(stocks.get(0).algorithmName, stocksCopy.get(0).algorithmName);
+		Assert.assertEquals(stocks.get(1).algorithmName, stocksCopy.get(1).algorithmName);
 	}
 }
