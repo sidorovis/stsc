27299085e78f75ceefff92e3699e6091bfc4a5dc
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-09-03 21:43:51 +0200

    #40 add SimulatorSettingsGridListWritable realisation + simple test

--- stsc.general/src/main/java/stsc/general/simulator/multistarter/grid/SimulatorSettingsGridList.java
+++ stsc.general/src/main/java/stsc/general/simulator/multistarter/grid/SimulatorSettingsGridList.java
@@ -16,8 +16,8 @@ public class SimulatorSettingsGridList implements Iterable<SimulatorSettings> {
 	protected final FromToPeriod period;
 	protected final boolean finished;
 
-	SimulatorSettingsGridList(StockStorage stockStorage, FromToPeriod period, List<GridExecutionInitializer> stocks, List<GridExecutionInitializer> eods,
-			boolean finished) {
+	public SimulatorSettingsGridList(StockStorage stockStorage, FromToPeriod period, List<GridExecutionInitializer> stocks,
+			List<GridExecutionInitializer> eods, boolean finished) {
 		this.stockStorage = stockStorage;
 		this.period = period;
 		this.stockInitializers = stocks;
