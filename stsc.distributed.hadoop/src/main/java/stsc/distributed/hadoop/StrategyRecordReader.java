f2508b7a015ac6a06da9cf0c2e65f1a629ba1cb8
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-09-04 21:54:39 +0200

    #40 added constructor to StrategyRecordReader

--- stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/StrategyRecordReader.java
+++ stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/StrategyRecordReader.java
@@ -1,8 +1,8 @@
 package stsc.distributed.hadoop;
 
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.mapreduce.RecordReader;
+public class StrategyRecordReader { // extends RecordReader<LongWritable, LongWritable> {
 
-public class StrategyRecordReader { // extends RecordReader<LongWritable, VALUEIN>{
+	public StrategyRecordReader() {
 
+	}
 }
