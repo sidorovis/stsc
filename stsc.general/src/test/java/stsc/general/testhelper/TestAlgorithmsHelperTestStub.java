76934789e165219219a33ade23ee18e83759b4e8
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-09-14 17:21:03 +0200

    #44 added Test Stub for general/testhelper project

---
+++ stsc.general/src/test/java/stsc/general/testhelper/TestAlgorithmsHelperTestStub.java
@@ -0,0 +1,9 @@
+package stsc.general.testhelper;
+
+import junit.framework.TestCase;
+
+public class TestAlgorithmsHelperTestStub extends TestCase {
+	public void testStub() {
+		assertNotNull(TestStstGeneralHelper.getEodAlgorithmInit());
+	}
+}
