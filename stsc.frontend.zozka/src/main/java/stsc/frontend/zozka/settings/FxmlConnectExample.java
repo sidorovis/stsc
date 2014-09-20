a5cb6ed495f1c49d5588a3a3ba1b8322d6658d9b
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-09-20 19:28:58 +0200

    #47 added SimulatorSettings dialog (with controller and interface description):

--- stsc.frontend.zozka/src/main/java/stsc/frontend/zozka/settings/FxmlConnectExample.java
+++ stsc.frontend.zozka/src/main/java/stsc/frontend/zozka/settings/FxmlConnectExample.java
@@ -1,30 +1,58 @@
 package stsc.frontend.zozka.settings;
 
+import java.io.IOException;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class FxmlConnectExample extends Application {
 
+	private CreateSettingsController createSettingsController;
+
 	@Override
 	public void start(final Stage stage) {
 		try {
-			final URL location = FxmlConnectExample.class.getResource("01_create_settings.fxml");
-			final BorderPane page = (BorderPane) FXMLLoader.load(location);
-			final Scene scene = new Scene(page);
-			stage.setScene(scene);
-			stage.setMinHeight(640);
-			stage.setMinWidth(610);
-			stage.setTitle("FXML is Simple");
-			stage.centerOnScreen();
-			stage.show();
+			if (createSimulatorSettingsDialog(stage)) {
+				createMainWindow(stage);
+			} else {
+				Platform.exit();
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
+			Platform.exit();
 		}
+	}
+
+	private void createMainWindow(Stage stage) {
+		stage.show();
+
+	}
+
+	private boolean createSimulatorSettingsDialog(final Stage stage)
+			throws IOException {
+		final Stage createSettingsStage = new Stage();
+		final URL location = FxmlConnectExample.class
+				.getResource("01_create_settings.fxml");
+		final FXMLLoader loader = new FXMLLoader();
+		final Parent createSettingsParent = loader.load(location.openStream());
+		createSettingsStage.initOwner(stage);
+		createSettingsStage.initModality(Modality.WINDOW_MODAL);
+		createSettingsController = loader.getController();
+		createSettingsController.setStage(createSettingsStage, stage);
+		final Scene scene = new Scene(createSettingsParent);
+		createSettingsStage.setScene(scene);
+		createSettingsStage.setMinHeight(660);
+		createSettingsStage.setMinWidth(615);
+		createSettingsStage.setTitle("Simulator Settings");
+		createSettingsStage.centerOnScreen();
+		createSettingsStage.showAndWait();
+		return createSettingsController.isValid();
 	}
 
 	public static void main(String[] args) {
