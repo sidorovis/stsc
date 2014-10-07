9a6a9e299f55c9fd0e5cccdf4c868b8cbe19dac5
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-10-07 23:00:11 +0200

    #85 fixed CostWeightedSumFunctionTest

--- stsc.general/src/test/java/stsc/general/statistic/cost/function/CostWeightedSumFunctionTest.java
+++ stsc.general/src/test/java/stsc/general/statistic/cost/function/CostWeightedSumFunctionTest.java
@@ -13,18 +13,18 @@ public class CostWeightedSumFunctionTest extends TestCase {
 
 		final CostWeightedSumFunction function = new CostWeightedSumFunction();
 		final Double result = function.calculate(statistics);
-		assertEquals(0.246987, result, Settings.doubleEpsilon);
+		assertEquals(-0.242883, result, Settings.doubleEpsilon);
 
 		function.addParameter("getPeriod", 0.5);
 		final Double result2 = function.calculate(statistics);
-		assertEquals(1.246987, result2, Settings.doubleEpsilon);
+		assertEquals(0.757116, result2, Settings.doubleEpsilon);
 
 		function.addParameter("getKelly", 0.3);
 		final Double result3 = function.calculate(statistics);
-		assertEquals(1.240465, result3, Settings.doubleEpsilon);
+		assertEquals(0.757116, result3, Settings.doubleEpsilon);
 
-		function.addParameter("getMaxWin", 0.7);
+		function.addParameter("getMaxLoss", 0.7);
 		final Double result4 = function.calculate(statistics);
-		assertEquals(49.540465, result4, Settings.doubleEpsilon);
+		assertEquals(127.457116, result4, Settings.doubleEpsilon);
 	}
 }
