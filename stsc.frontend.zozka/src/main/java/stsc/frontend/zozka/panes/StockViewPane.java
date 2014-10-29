0cecffce6740913a444420122b41055d3b4a5799
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-10-29 01:09:13 +0100

    #90 clean up several unused imports

--- stsc.frontend.zozka/src/main/java/stsc/frontend/zozka/panes/StockViewPane.java
+++ stsc.frontend.zozka/src/main/java/stsc/frontend/zozka/panes/StockViewPane.java
@@ -39,7 +39,6 @@ import javafx.scene.Parent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
