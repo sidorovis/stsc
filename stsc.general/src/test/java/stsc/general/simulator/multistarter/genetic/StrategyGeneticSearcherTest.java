8c7b864e877f28f39f17547aff427bd0b7c30c00
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-11-24 20:16:24 +0100

    #42 fixed StrategyGeneticSearcherTest (changed number for avGain) moved to JUnit4

--- stsc.general/src/test/java/stsc/general/simulator/multistarter/genetic/StrategyGeneticSearcherTest.java
+++ stsc.general/src/test/java/stsc/general/simulator/multistarter/genetic/StrategyGeneticSearcherTest.java
@@ -3,6 +3,9 @@ package stsc.general.simulator.multistarter.genetic;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import stsc.common.Settings;
 import stsc.general.simulator.multistarter.StrategySearcher.IndicatorProgressListener;
 import stsc.general.simulator.multistarter.StrategySearcherException;
@@ -10,24 +13,27 @@ import stsc.general.statistic.StatisticsByCostSelector;
 import stsc.general.statistic.StrategySelector;
 import stsc.general.statistic.cost.function.CostWeightedSumFunction;
 import stsc.general.testhelper.TestGeneticSimulatorSettings;
-import junit.framework.TestCase;
 
-public class StrategyGeneticSearcherTest extends TestCase {
+public class StrategyGeneticSearcherTest {
 
+	@Test
 	public void testStrategyGeneticSearcher() throws InterruptedException, StrategySearcherException {
 		final StrategyGeneticSearcher sgs = createSearcher();
 		final StrategySelector selector = sgs.getSelector();
-		assertEquals(112, selector.getStrategies().size());
-		assertEquals(34.911532, selector.getStrategies().get(0).getStatistics().getAvGain(), Settings.doubleEpsilon);
+		Assert.assertEquals(112, selector.getStrategies().size());
+		Assert.assertEquals(100.0, selector.getStrategies().get(0).getStatistics().getAvGain(), Settings.doubleEpsilon);
+		Assert.assertEquals(0.666666, selector.getStrategies().get(0).getStatistics().getWinProb(), Settings.doubleEpsilon);
 	}
 
+	@Test
 	public void testStrategyGeneticSearchStop() throws InterruptedException, StrategySearcherException {
 		final StrategyGeneticSearcher sgs = createSearcher();
 		sgs.stopSearch();
 		final StrategySelector selector = sgs.getSelector();
-		assertTrue(100 > selector.getStrategies().size());
+		Assert.assertTrue(100 > selector.getStrategies().size());
 	}
 
+	@Test
 	public void testStrategySearchProcessingListener() throws InterruptedException, StrategySearcherException {
 		final StrategyGeneticSearcher sgs = createSearcher();
 		final List<Double> updates = new ArrayList<>();
@@ -39,8 +45,8 @@ public class StrategyGeneticSearcherTest extends TestCase {
 		});
 		final StrategySelector selector = sgs.getSelector();
 
-		assertEquals(112, selector.getStrategies().size());
-		assertEquals(104, updates.size());
+		Assert.assertEquals(112, selector.getStrategies().size());
+		Assert.assertEquals(104, updates.size());
 	}
 
 	private StrategyGeneticSearcher createSearcher() throws InterruptedException {
