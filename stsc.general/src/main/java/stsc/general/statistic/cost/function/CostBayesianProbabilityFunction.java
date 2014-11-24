7e0f59578849816d93fd9ae05829dcb7c03b8c91
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-11-24 19:40:13 +0100

    #42 fixed CostBayesianProbabilityFunction test (after datafeed adjustment on adjective close price) and added description for CostBayesianProbabilityFunction

--- stsc.general/src/main/java/stsc/general/statistic/cost/function/CostBayesianProbabilityFunction.java
+++ stsc.general/src/main/java/stsc/general/statistic/cost/function/CostBayesianProbabilityFunction.java
@@ -8,6 +8,19 @@ import java.util.Map.Entry;
 
 import stsc.general.statistic.Statistics;
 
+// @formatter:off
+/**
+ * Calculate Cost Function for {@link Statistics} using Bayesian Probability
+ * methodology.
+ * Parameters are divided by layers with different coefficients for example:
+ * we could have layer with next coefficients (getPeriod -> 2.0, getAvGain -> 4.0)
+ * which provide us with information that cost function for such layer will be:
+ * max(Period * 2.0 and AvGain * 4.0);
+ * Result of function is minimum between all layers.
+ * If there is no layers result is Double.MAX_VALUE
+ * layer with no fields lead to get -Double.MAX_VALUE as layer value.
+ */
+//@formatter:on
 public class CostBayesianProbabilityFunction implements CostFunction {
 
 	private final List<Map<String, Double>> parameters = new ArrayList<Map<String, Double>>();
--- stsc.general/src/test/java/stsc/general/statistic/cost/function/CostBayesianProbabilityFunctionTest.java
+++ stsc.general/src/test/java/stsc/general/statistic/cost/function/CostBayesianProbabilityFunctionTest.java
@@ -9,6 +9,10 @@ import junit.framework.TestCase;
 
 public class CostBayesianProbabilityFunctionTest extends TestCase {
 
+	private Double calculateTestValue(Statistics statistics) {
+		return Math.min(statistics.getPeriod() * 6.0, statistics.getAvGain() * 11.0);
+	}
+	
 	public void testBayesianProbabilityCostFunction() throws ParseException {
 		final Statistics statistics = TestStatisticsHelper.getStatistics();
 
@@ -16,6 +20,6 @@ public class CostBayesianProbabilityFunctionTest extends TestCase {
 		bayesian.addLayer().put("getPeriod", 6.0);
 		bayesian.addLayer().put("getAvGain", 11.0);
 		final Double bayesianResult = bayesian.calculate(statistics);
-		assertEquals(-4.286288, bayesianResult, Settings.doubleEpsilon);
+		assertEquals(calculateTestValue(statistics), bayesianResult, Settings.doubleEpsilon);
 	}
 }
