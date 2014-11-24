4cd980b2645a21aad98003068fa190813555a3c7
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-11-24 22:54:22 +0100

    #42 fixed DiffTest added calculation but not value check and moved to JUnit

--- stsc.integration.tests/src/test/java/stsc/integration/tests/algorithms/stock/factors/primitive/DiffTest.java
+++ stsc.integration.tests/src/test/java/stsc/integration/tests/algorithms/stock/factors/primitive/DiffTest.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import java.util.ArrayList;
 
 import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
 
 import stsc.algorithms.In;
 import stsc.algorithms.stock.factors.primitive.Diff;
@@ -12,32 +14,37 @@ import stsc.algorithms.stock.factors.primitive.Ema;
 import stsc.algorithms.stock.factors.primitive.Sma;
 import stsc.common.BadSignalException;
 import stsc.common.Day;
+import stsc.common.Settings;
 import stsc.common.algorithms.BadAlgorithmException;
 import stsc.common.stocks.Stock;
 import stsc.common.stocks.UnitedFormatStock;
+import stsc.common.storage.SignalsStorage;
 import stsc.integration.tests.helper.StockAlgoInitHelper;
 import stsc.signals.DoubleSignal;
-import junit.framework.TestCase;
 
-public class DiffTest extends TestCase {
+public class DiffTest {
+
+	@Test
 	public void testDiff() throws BadAlgorithmException, IOException, BadSignalException, ParseException {
 
-		StockAlgoInitHelper stockInit = new StockAlgoInitHelper("in", "aapl");
+		final StockAlgoInitHelper stockInit = new StockAlgoInitHelper("in", "aapl");
 		stockInit.getSettings().setString("e", "open");
 		final In in = new In(stockInit.getInit());
 
-		StockAlgoInitHelper emaInit = new StockAlgoInitHelper("ema", "aapl", stockInit.getStorage());
+		final StockAlgoInitHelper emaInit = new StockAlgoInitHelper("ema", "aapl", stockInit.getStorage());
 		emaInit.getSettings().addSubExecutionName("in");
-		Ema ema = new Ema(emaInit.getInit());
+		emaInit.getSettings().setInteger("size", 10000);
+		final Ema ema = new Ema(emaInit.getInit());
 
-		StockAlgoInitHelper smaInit = new StockAlgoInitHelper("sma", "aapl", stockInit.getStorage());
+		final StockAlgoInitHelper smaInit = new StockAlgoInitHelper("sma", "aapl", stockInit.getStorage());
 		smaInit.getSettings().addSubExecutionName("in");
-		Sma sma = new Sma(smaInit.getInit());
+		smaInit.getSettings().setInteger("size", 10000);
+		final Sma sma = new Sma(smaInit.getInit());
 
-		StockAlgoInitHelper diffInit = new StockAlgoInitHelper("diff", "aapl", stockInit.getStorage());
+		final StockAlgoInitHelper diffInit = new StockAlgoInitHelper("diff", "aapl", stockInit.getStorage());
 		diffInit.getSettings().addSubExecutionName("ema").addSubExecutionName("sma");
 		diffInit.getSettings().setInteger("size", 10000);
-		Diff diff = new Diff(diffInit.getInit());
+		final Diff diff = new Diff(diffInit.getInit());
 
 		final Stock aapl = UnitedFormatStock.readFromUniteFormatFile("./test_data/aapl.uf");
 		final int aaplIndex = aapl.findDayIndex(new LocalDate(2011, 9, 4).toDate());
@@ -51,8 +58,14 @@ public class DiffTest extends TestCase {
 			diff.process(day);
 		}
 
-		assertNotNull(stockInit.getStorage().getStockSignal("aapl", "diff", 0));
-		assertEquals(0.0, stockInit.getStorage().getStockSignal("aapl", "diff", 0).getSignal(DoubleSignal.class).value, 0.001);
-		assertEquals(0.928176, stockInit.getStorage().getStockSignal("aapl", "diff", 4).getSignal(DoubleSignal.class).value, 0.001);
+		final SignalsStorage ss = stockInit.getStorage();
+		Assert.assertNotNull(ss.getStockSignal("aapl", "diff", 0));
+		Assert.assertEquals(0.0, ss.getStockSignal("aapl", "diff", 0).getSignal(DoubleSignal.class).value, Settings.doubleEpsilon);
+
+		final double emaValue = ss.getStockSignal("aapl", "ema", 4).getSignal(DoubleSignal.class).value;
+		final double smaValue = ss.getStockSignal("aapl", "sma", 4).getSignal(DoubleSignal.class).value;
+
+		Assert.assertEquals(emaValue - smaValue, ss.getStockSignal("aapl", "diff", 4).getSignal(DoubleSignal.class).value,
+				Settings.doubleEpsilon);
 	}
 }
