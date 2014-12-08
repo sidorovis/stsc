6ed1047d838e612d585db85c0f690220bcc94537
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-12-08 21:46:27 +0100

    #51 added AtrTrueRange index prototype

---
+++ stsc.algorithms/src/main/java/stsc/algorithms/stock/indices/atr/AtrTrueRange.java
@@ -0,0 +1,31 @@
+package stsc.algorithms.stock.indices.atr;
+
+import stsc.common.BadSignalException;
+import stsc.common.Day;
+import stsc.common.algorithms.BadAlgorithmException;
+import stsc.common.algorithms.StockAlgorithm;
+import stsc.common.algorithms.StockAlgorithmInit;
+import stsc.common.signals.SignalsSerie;
+import stsc.common.signals.StockSignal;
+import stsc.signals.DoubleSignal;
+import stsc.signals.series.LimitSignalsSerie;
+
+public class AtrTrueRange extends StockAlgorithm {
+
+	public AtrTrueRange(StockAlgorithmInit init) throws BadAlgorithmException {
+		super(init);
+	}
+
+	@Override
+	public SignalsSerie<StockSignal> registerSignalsClass(StockAlgorithmInit initialize) throws BadAlgorithmException {
+		final int size = initialize.getSettings().getIntegerSetting("size", 2).getValue().intValue();
+		return new LimitSignalsSerie<>(DoubleSignal.class, size);
+	}
+
+	@Override
+	public void process(Day day) throws BadSignalException {
+		// TODO finish this algorithm
+
+	}
+
+}
