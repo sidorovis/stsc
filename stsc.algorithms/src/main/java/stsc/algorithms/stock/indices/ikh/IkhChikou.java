62b2f92fbd8dfd53410075d4dd74311c684f8b4e
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-12-14 17:07:41 +0100

    #49 added IkhChikou and IkhSenkouB with tests and documentation to README.md

---
+++ stsc.algorithms/src/main/java/stsc/algorithms/stock/indices/ikh/IkhChikou.java
@@ -0,0 +1,42 @@
+package stsc.algorithms.stock.indices.ikh;
+
+import java.util.LinkedList;
+
+import stsc.common.BadSignalException;
+import stsc.common.Day;
+import stsc.common.algorithms.BadAlgorithmException;
+import stsc.common.algorithms.StockAlgorithm;
+import stsc.common.algorithms.StockAlgorithmInit;
+import stsc.common.signals.SignalsSerie;
+import stsc.common.signals.StockSignal;
+import stsc.signals.DoubleSignal;
+import stsc.signals.series.LimitSignalsSerie;
+
+public class IkhChikou extends StockAlgorithm {
+
+	private final int tm;
+	private final LinkedList<Double> closes = new LinkedList<>();
+
+	public IkhChikou(StockAlgorithmInit init) throws BadAlgorithmException {
+		super(init);
+		tm = init.getSettings().getIntegerSetting("TM", 26).getValue();
+	}
+
+	@Override
+	public SignalsSerie<StockSignal> registerSignalsClass(StockAlgorithmInit initialize) throws BadAlgorithmException {
+		final int size = initialize.getSettings().getIntegerSetting("size", 2).getValue();
+		return new LimitSignalsSerie<>(DoubleSignal.class, size);
+	}
+
+	@Override
+	public void process(Day day) throws BadSignalException {
+		final int currentIndex = getCurrentIndex();
+		closes.addFirst(day.getPrices().getClose());
+		if (currentIndex < tm) {
+			addSignal(day.getDate(), new DoubleSignal(closes.getLast()));
+		} else {
+			final double close = closes.pollLast();
+			addSignal(day.getDate(), new DoubleSignal(close));
+		}
+	}
+}
---
+++ stsc.algorithms/src/main/java/stsc/algorithms/stock/indices/ikh/IkhSenkauB.java
@@ -0,0 +1,40 @@
+package stsc.algorithms.stock.indices.ikh;
+
+import stsc.algorithms.AlgorithmSettingsImpl;
+import stsc.common.BadSignalException;
+import stsc.common.Day;
+import stsc.common.algorithms.BadAlgorithmException;
+import stsc.common.algorithms.StockAlgorithm;
+import stsc.common.algorithms.StockAlgorithmInit;
+import stsc.common.signals.SignalsSerie;
+import stsc.common.signals.StockSignal;
+import stsc.signals.DoubleSignal;
+import stsc.signals.series.LimitSignalsSerie;
+
+public class IkhSenkauB extends StockAlgorithm {
+
+	private final String prototypeName;
+	private final IkhPrototype prototype;
+
+	public IkhSenkauB(StockAlgorithmInit init) throws BadAlgorithmException {
+		super(init);
+		this.prototypeName = init.getExecutionName() + "_IhkPrototype";
+		final AlgorithmSettingsImpl settings = new AlgorithmSettingsImpl(init);
+		settings.setInteger("TS", init.getSettings().getIntegerSetting("TL", 52).getValue());
+		settings.setInteger("TM", init.getSettings().getIntegerSetting("TM", 26).getValue());
+		this.prototype = new IkhPrototype(init.createInit(prototypeName, settings));
+	}
+
+	@Override
+	public SignalsSerie<StockSignal> registerSignalsClass(StockAlgorithmInit initialize) throws BadAlgorithmException {
+		final int size = initialize.getSettings().getIntegerSetting("size", 2).getValue();
+		return new LimitSignalsSerie<>(DoubleSignal.class, size);
+	}
+
+	@Override
+	public void process(Day day) throws BadSignalException {
+		prototype.process(day);
+		final DoubleSignal v = getSignal(prototypeName, day.getDate()).getSignal(DoubleSignal.class);
+		addSignal(day.getDate(), v);
+	}
+}
