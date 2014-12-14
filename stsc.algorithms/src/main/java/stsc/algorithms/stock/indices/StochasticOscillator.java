5ae0799c2d613d25e0d9a10b8136f7542a9952cd
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-12-14 23:53:21 +0100

    #76 added stochastic oscillator with tests and documentation to README.md

--- stsc.algorithms/src/main/java/stsc/algorithms/stock/indices/StochasticOscillator.java
+++ stsc.algorithms/src/main/java/stsc/algorithms/stock/indices/StochasticOscillator.java
@@ -1,5 +1,7 @@
 package stsc.algorithms.stock.indices;
 
+import stsc.algorithms.AlgorithmSettingsImpl;
+import stsc.algorithms.Input;
 import stsc.common.BadSignalException;
 import stsc.common.Day;
 import stsc.common.algorithms.BadAlgorithmException;
@@ -12,9 +14,60 @@ import stsc.signals.series.LimitSignalsSerie;
 
 public class StochasticOscillator extends StockAlgorithm {
 
+	private final String lnInputName;
+	private final Input lnInput;
+
+	private final String lnName;
+	private final MinForNDays ln;
+
+	private final String hnInputName;
+	private final Input hnInput;
+
+	private final String hnName;
+	private final MaxForNDays hn;
+
 	public StochasticOscillator(StockAlgorithmInit init) throws BadAlgorithmException {
 		super(init);
-		// TODO Auto-generated constructor stub
+
+		this.lnInputName = init.getExecutionName() + "_LnInput";
+		this.lnInput = createLnInput(init);
+
+		this.lnName = init.getExecutionName() + "_Ln";
+		this.ln = createLn(init);
+
+		this.hnInputName = init.getExecutionName() + "_HnInput";
+		this.hnInput = createHnInput(init);
+
+		this.hnName = init.getExecutionName() + "_Hn";
+		this.hn = createHn(init);
+	}
+
+	private Input createLnInput(StockAlgorithmInit init) throws BadAlgorithmException {
+		final AlgorithmSettingsImpl settings = new AlgorithmSettingsImpl(init);
+		settings.setString("e", "low");
+		return new Input(init.createInit(lnInputName, settings));
+	}
+
+	private MinForNDays createLn(StockAlgorithmInit init) throws BadAlgorithmException {
+		final AlgorithmSettingsImpl settings = new AlgorithmSettingsImpl(init);
+		settings.setInteger("P", init.getSettings().getIntegerSetting("P", 5).getValue());
+		settings.setInteger("SP", init.getSettings().getIntegerSetting("SP", 0).getValue());
+		settings.addSubExecutionName(lnInputName);
+		return new MinForNDays(init.createInit(lnName, settings));
+	}
+
+	private Input createHnInput(StockAlgorithmInit init) throws BadAlgorithmException {
+		final AlgorithmSettingsImpl settings = new AlgorithmSettingsImpl(init);
+		settings.setString("e", "high");
+		return new Input(init.createInit(hnInputName, settings));
+	}
+
+	private MaxForNDays createHn(StockAlgorithmInit init) throws BadAlgorithmException {
+		final AlgorithmSettingsImpl settings = new AlgorithmSettingsImpl(init);
+		settings.setInteger("P", init.getSettings().getIntegerSetting("P", 5).getValue());
+		settings.setInteger("SP", init.getSettings().getIntegerSetting("SP", 0).getValue());
+		settings.addSubExecutionName(hnInputName);
+		return new MaxForNDays(init.createInit(hnName, settings));
 	}
 
 	@Override
@@ -25,8 +78,19 @@ public class StochasticOscillator extends StockAlgorithm {
 
 	@Override
 	public void process(Day day) throws BadSignalException {
-		final double ct = day.getPrices().getClose();
+		lnInput.process(day);
+		ln.process(day);
+		hnInput.process(day);
+		hn.process(day);
+		final double ctValue = day.getPrices().getClose();
+		final double lnValue = getSignal(lnName, day.getDate()).getSignal(DoubleSignal.class).getValue();
+		final double hnValue = getSignal(hnName, day.getDate()).getSignal(DoubleSignal.class).getValue();
 
+		if (Double.compare(hnValue, lnValue) == 0) {
+			addSignal(day.getDate(), new DoubleSignal(50.0));
+		} else {
+			addSignal(day.getDate(), new DoubleSignal(100.0 * (ctValue - lnValue) / (hnValue - lnValue)));
+		}
 	}
 
 }
