80d535bf956568a9a18c9c1e50b18d5aac116428
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-12-18 22:30:08 +0100

    #70 added AdlAdln AdlAdlt algorithms and documentation README.md

--- README.md
+++ README.md
@@ -30,6 +30,19 @@ e -> [-SubAlgoName-]}
 -> DoubleSignal.
 ```
 ##### stsc.algorithms.eod.indices.adl
+ * AdlAdl
+```
+{ {no_input},
+e -> [Integer: size (2 by default)]}
+-> DoubleSignal.
+```
+ * AdlAdln
+```
+{ {no_input},
+e -> [Integer: size (2 by default)]}
+-> DoubleSignal.
+```
+ * AdlAdlt
 ```
 { {no_input},
 e -> [Integer: size (2 by default)]}
---
+++ stsc.algorithms/src/main/java/stsc/algorithms/eod/indices/adl/AdlAdln.java
@@ -0,0 +1,64 @@
+package stsc.algorithms.eod.indices.adl;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import stsc.common.BadSignalException;
+import stsc.common.Day;
+import stsc.common.algorithms.BadAlgorithmException;
+import stsc.common.algorithms.EodAlgorithm;
+import stsc.common.algorithms.EodAlgorithmInit;
+import stsc.common.signals.EodSignal;
+import stsc.common.signals.SignalsSerie;
+import stsc.signals.eod.EodDoubleSignal;
+import stsc.signals.series.LimitSignalsSerie;
+
+public class AdlAdln extends EodAlgorithm {
+
+	private final Map<String, Double> lastPrices = new HashMap<>();
+
+	public AdlAdln(EodAlgorithmInit init) throws BadAlgorithmException {
+		super(init);
+	}
+
+	@Override
+	public SignalsSerie<EodSignal> registerSignalsClass(EodAlgorithmInit init) throws BadAlgorithmException {
+		final int size = init.getSettings().getIntegerSetting("size", 2).getValue().intValue();
+		return new LimitSignalsSerie<>(EodDoubleSignal.class, size);
+	}
+
+	@Override
+	public void process(Date date, HashMap<String, Day> datafeed) throws BadSignalException {
+		double at = 0;
+		double dt = 0;
+		for (Entry<String, Day> e : datafeed.entrySet()) {
+			final Day d = e.getValue();
+			final Double close = d.getPrices().getClose();
+			Double v = lastPrices.get(e.getKey());
+			if (v == null) {
+				v = close;
+			}
+			lastPrices.put(e.getKey(), close);
+			if (close >= v) {
+				at += 1;
+			} else {
+				dt += 1;
+			}
+		}
+		final int index = getCurrentIndex();
+		if (index == 0) {
+			addSignal(date, new EodDoubleSignal(0.0));
+		} else {
+			if (Double.compare(at + dt, 0.0) == 0) {
+				addSignal(date, new EodDoubleSignal(getSignal(index - 1).getSignal(EodDoubleSignal.class).getValue()));
+			} else {
+
+			}
+			final double newAdditional = (at - dt) + (at + dt);
+			addSignal(date, new EodDoubleSignal(newAdditional + getSignal(index - 1).getSignal(EodDoubleSignal.class).getValue()));
+		}
+	}
+
+}
