f4c8829e80e007cf643c36da8c2dddec238ee5ef
Ivan Sidarau <ivan.sidarau@gmail.com>
2014-12-23 23:44:53 +0100

    #67 added Least Squares Straight algorithm

--- stsc.algorithms/src/main/java/stsc/algorithms/stock/geometry/LeastSquaresStraight.java
+++ stsc.algorithms/src/main/java/stsc/algorithms/stock/geometry/LeastSquaresStraight.java
@@ -1,9 +1,7 @@
 package stsc.algorithms.stock.geometry;
 
 import java.util.LinkedList;
-import java.util.List;
 
-import stsc.algorithms.ListOfDoubleAdapter;
 import stsc.common.BadSignalException;
 import stsc.common.Day;
 import stsc.common.algorithms.BadAlgorithmException;
@@ -21,7 +19,12 @@ public class LeastSquaresStraight extends StockAlgorithm {
 	private final String subExecutionName;
 
 	private double currentX = 0.0;
-	private final List<Double> y = new LinkedList<>();
+	private final LinkedList<Double> y = new LinkedList<>();
+
+	private double sumY = 0.0;
+	private double sumX = 0.0;
+	private double sumSqrX = 0.0;
+	private double sumXY = 0.0;
 
 	public LeastSquaresStraight(StockAlgorithmInit init) throws BadAlgorithmException {
 		super(init);
@@ -36,8 +39,9 @@ public class LeastSquaresStraight extends StockAlgorithm {
 	public SignalsSerie<SerieSignal> registerSignalsClass(StockAlgorithmInit initialize) throws BadAlgorithmException {
 		final int size = initialize.getSettings().getIntegerSetting("size", 2).getValue().intValue();
 		return new LimitSignalsSerie<>(ListOfDoubleSignal.class, size);
-		// double get by index 0 -> probability
-		// double get by index 1 -> angle
+		// y = a0 + a1 * x + et
+		// double get by index 1 -> a0
+		// double get by index 2 -> a1
 	}
 
 	@Override
@@ -45,11 +49,32 @@ public class LeastSquaresStraight extends StockAlgorithm {
 		currentX += 1;
 		final double yValue = getSignal(subExecutionName, day.getDate()).getSignal(DoubleSignal.class).getValue();
 		y.add(yValue);
-		final ListOfDoubleSignal signal = new ListOfDoubleSignal();
-		if (currentX == N) {
-			
-		} else if (currentX > N) {
 
+		sumY += yValue;
+		sumSqrX += (currentX * currentX);
+		sumX += currentX;
+		sumXY += currentX * yValue;
+
+		final ListOfDoubleSignal signal = new ListOfDoubleSignal();
+		if (currentX >= N) {
+			final double oldX = currentX - N + 1;
+			final double oldY = y.pollFirst();
+			sumY -= oldY;
+			sumX -= oldX;
+			sumSqrX -= (oldX * oldX);
+			sumXY -= (oldX * oldY);
+			final double divider = (N * sumSqrX - (sumX * sumX));
+			if (Double.compare(divider, 0.0) == 0) {
+				signal.add(0.0);
+				signal.add(0.0);
+				addSignal(day.getDate(), signal);
+			} else {
+				final double a0 = (sumY * sumSqrX - sumX * sumXY) / divider;
+				final double a1 = (N * sumXY - sumY * sumX) / divider;
+				signal.add(a0);
+				signal.add(a1);
+				addSignal(day.getDate(), signal);
+			}
 		} else {
 			signal.add(0.0);
 			signal.add(0.0);
