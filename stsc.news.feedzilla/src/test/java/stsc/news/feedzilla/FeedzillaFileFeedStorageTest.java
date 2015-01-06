f3fc0a548014260d21577a9bfeeffcc2dd01c5f9
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-01-06 22:58:54 +0100

    #107 add simple check for FeedzelliFileFeedStorageTest

--- stsc.news.feedzilla/src/test/java/stsc/news/feedzilla/FeedzillaFileFeedStorageTest.java
+++ stsc.news.feedzilla/src/test/java/stsc/news/feedzilla/FeedzillaFileFeedStorageTest.java
@@ -4,10 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class FeedzillaFileFeedStorageTest {
-
 	@Test
 	public void testFeedzillaFileFeedStorage() {
-		// FeedzillaNewsDatafeed fz = new FeedzillaNewsDatafeed();
-		// Assert.assertNotNull(fz);
+		final FeedzillaFileFeedStorage fffs = new FeedzillaFileFeedStorage();
+		Assert.assertNotNull(fffs);
 	}
 }
