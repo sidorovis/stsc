7552edeeb9a474609d3780438cf87c15579d88fd
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-01-06 23:16:51 +0100

    #106 added thread safe FeedzillaFileFeedStorage

--- stsc.common/src/main/java/stsc/common/storage/FeedStorage.java
+++ stsc.common/src/main/java/stsc/common/storage/FeedStorage.java
@@ -1,9 +1,13 @@
 package stsc.common.storage;
 
+import java.util.List;
+
+import org.joda.time.DateTime;
+
 import stsc.common.feeds.Feed;
 
 public interface FeedStorage {
 
-	public Feed getFeed(String feedName);
+	public List<Feed> getFeeds(final DateTime dateTime);
 
 }
--- stsc.news.feedzilla/src/main/java/stsc/news/feedzilla/FeedzillaFileFeedStorage.java
+++ stsc.news.feedzilla/src/main/java/stsc/news/feedzilla/FeedzillaFileFeedStorage.java
@@ -1,14 +1,46 @@
 package stsc.news.feedzilla;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+
 import stsc.common.feeds.Feed;
 import stsc.common.storage.FeedStorage;
 
 public class FeedzillaFileFeedStorage implements FeedStorage {
 
-	@Override
-	public Feed getFeed(String feedName) {
+	private Map<DateTime, List<Feed>> datafeed = Collections.synchronizedMap(new HashMap<>());
+
+	public FeedzillaFileFeedStorage(String feedFolder) {
+		readFeedzillaData();
+	}
+
+	private void readFeedzillaData() {
 		// TODO Auto-generated method stub
-		return null;
+
+	}
+
+	public void addFeed(final Feed newFeed) {
+		final DateTime publishDate = newFeed.getArticle().getPublishDate();
+		final List<Feed> feedList = datafeed.get(publishDate);
+		synchronized (datafeed) {
+			if (feedList == null) {
+				final List<Feed> newFeedList = Collections.synchronizedList(new ArrayList<>());
+				newFeedList.add(newFeed);
+				datafeed.put(publishDate, newFeedList);
+			} else {
+				feedList.add(newFeed);
+			}
+		}
+	}
+
+	@Override
+	public List<Feed> getFeeds(final DateTime dateTime) {
+		return datafeed.get(dateTime);
 	}
 
 }
