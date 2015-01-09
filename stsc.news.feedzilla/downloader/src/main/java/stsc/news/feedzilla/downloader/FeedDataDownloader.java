fe98bd4d3b326eb1e6b9d8f8827c0a5d6a508f61
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-01-10 00:43:10 +0100

    #108 added category and subcategory tables to liquibase migration mechanism

--- stsc.news.feedzilla/downloader/src/main/java/stsc/news/feedzilla/downloader/FeedDataDownloader.java
+++ stsc.news.feedzilla/downloader/src/main/java/stsc/news/feedzilla/downloader/FeedDataDownloader.java
@@ -30,40 +30,61 @@ final class FeedDataDownloader {
 	}
 
 	FeedDataDownloader() {
-		DateTime startOfDay = DateTime.now();
-		startOfDay = startOfDay.minusYears(15);
-		startOfDay = startOfDay.withTimeAtStartOfDay();
+		// DateTime startOfDay = DateTime.now();
+		// startOfDay = startOfDay.minusYears(15);
+		// startOfDay = startOfDay.withTimeAtStartOfDay();
 		final FeedZilla feed = new FeedZilla();
-		int i = 0;
-		final HashMap<String, Article> map = new HashMap<>();
-		final List<Category> categories = feed.getCategories();
-		for (Category c : categories) {
-			final List<Subcategory> subcategories = feed.getSubcategories(c);
-			System.out.println(" - " + subcategories.size());
-			pause();
-			// for (Subcategory s : subcategories) {
-			// try {
-			// final Articles articles =
-			// feed.query().category(c).subcategory(s).since(startOfDay).count(100).articles();
-			// if (articles == null) {
-			// System.out.println("--------- -------------");
-			// } else {
-			// System.out.println("Size: " + articles.getArticles().size());
-			// // for (Article article : articles.getArticles()) {
-			// // i++;
-			// // map.put(article.getTitle(), article);
-			// // pause();
-			// // if (i % 1000 == 0) {
-			// // System.out.println("articles " + i);
-			// // }
-			// // }
-			// }
-			// } catch (Exception e) {
-			// System.err.println(i++ + " " + e.getMessage());
-			// }
-			// }
+		// int i = 0;
+		// final HashMap<String, Article> map = new HashMap<>();
+
+		int maxDsn = 0;
+		int maxEsn = 0;
+		int maxUsn = 0;
+
+		final List<Subcategory> categories = feed.getSubcategories();
+		for (Subcategory subcategory : categories) {
+			System.out.println(subcategory.getParentId());
+			maxDsn = Math.max(maxDsn, subcategory.getDisplayName().length());
+			maxEsn = Math.max(maxDsn, subcategory.getEnglishName().length());
+			maxUsn = Math.max(maxDsn, subcategory.getUrlName().length());
 		}
-		System.out.println("Size is: " + i + " by map: " + map.size());
+		System.out.println("-----------");
+		System.out.println(maxDsn);
+		System.out.println(maxEsn);
+		System.out.println(maxUsn);
+
+		// for (Category c : categories) {
+		// System.out.println(c.getId());
+		// System.out.println(c.getDisplayName());
+		// System.out.println(c.getEnglishName());
+		// System.out.println(c.getUrlName());
+		// }
+		// final List<Subcategory> subcategories = feed.getSubcategories(c);
+		// System.out.println(" - " + subcategories.size());
+		// pause();
+		// for (Subcategory s : subcategories) {
+		// try {
+		// final Articles articles =
+		// feed.query().category(c).subcategory(s).since(startOfDay).count(100).articles();
+		// if (articles == null) {
+		// System.out.println("--------- -------------");
+		// } else {
+		// System.out.println("Size: " + articles.getArticles().size());
+		// // for (Article article : articles.getArticles()) {
+		// // i++;
+		// // map.put(article.getTitle(), article);
+		// // pause();
+		// // if (i % 1000 == 0) {
+		// // System.out.println("articles " + i);
+		// // }
+		// // }
+		// }
+		// } catch (Exception e) {
+		// System.err.println(i++ + " " + e.getMessage());
+		// }
+		// }
+		// }
+		// System.out.println("Size is: " + i + " by map: " + map.size());
 
 	}
 
