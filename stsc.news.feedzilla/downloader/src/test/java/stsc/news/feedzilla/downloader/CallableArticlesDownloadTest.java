eefeb1e28defaf59ccdf60066348cc42417c679a
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-01-18 22:35:29 +0100

    #122 added test for CallableArticlesDownloadTest

---
+++ stsc.news.feedzilla/downloader/src/test/java/stsc/news/feedzilla/downloader/CallableArticlesDownloadTest.java
@@ -0,0 +1,32 @@
+package stsc.news.feedzilla.downloader;
+
+import java.util.List;
+import java.util.Optional;
+
+import graef.feedzillajava.Article;
+import graef.feedzillajava.Category;
+import graef.feedzillajava.FeedZilla;
+import graef.feedzillajava.Subcategory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CallableArticlesDownloadTest {
+
+	private final Logger logger = LogManager.getLogger(CallableArticlesDownloadTest.class);
+
+	@Test
+	public void testCallableArticlesDownload() throws Exception {
+		final FeedZilla feed = new FeedZilla();
+		final Category c = feed.getCategories().get(0);
+		final Subcategory s = feed.getSubcategories(c).get(0);
+		final CallableArticlesDownload callable = new CallableArticlesDownload(logger, feed, c, s, 1, new DateTime().minusDays(10));
+
+		final Optional<List<Article>> articles = callable.call();
+		Assert.assertTrue(articles.isPresent());
+		Assert.assertFalse(articles.get().isEmpty());
+	}
+}
