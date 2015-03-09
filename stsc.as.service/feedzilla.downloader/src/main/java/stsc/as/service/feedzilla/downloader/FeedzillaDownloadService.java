f276b303d0c967d0ec885b03538d802b40c87dfc
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-03-09 19:46:17 +0100

    #136 added more informative message bfore starting processing cycle (download) at FeedzillaDownloadService

--- stsc.as.service/feedzilla.downloader/src/main/java/stsc/as/service/feedzilla/downloader/FeedzillaDownloadService.java
+++ stsc.as.service/feedzilla.downloader/src/main/java/stsc/as/service/feedzilla/downloader/FeedzillaDownloadService.java
@@ -1,5 +1,11 @@
 package stsc.as.service.feedzilla.downloader;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.TimeZone;
 import java.util.logging.Level;
 
@@ -8,7 +14,12 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.XMLConfigurationFactory;
 
 import stsc.common.service.ApplicationHelper;
+import stsc.common.service.FeedzillaDownloaderSettings;
 import stsc.common.service.StopableApp;
+import stsc.common.service.statistics.StatisticType;
+import stsc.database.migrations.FeedzillaDownloaderDatabaseSettings;
+import stsc.database.service.schemas.OrmliteFeedzillaDownloaderLogger;
+import stsc.database.service.storages.FeedzillaDownloaderDatabaseStorage;
 
 final class FeedzillaDownloadService implements StopableApp {
 
@@ -18,9 +29,33 @@ final class FeedzillaDownloadService implements StopableApp {
 	}
 
 	private final Logger logger = LogManager.getLogger(FeedzillaDownloadService.class.getName());
-	
-	public FeedzillaDownloadService() {
 
+	private final String settingName = "feedzilla_downloader";
+
+	private volatile boolean stopped = false;
+	private FeedzillaDownloaderSettings settings;
+	private final FeedzillaDownloaderDatabaseStorage settingsStorage;
+	private final OrmliteFeedzillaDownloaderLogger downloaderLogger;
+
+	// private Optional<YahooDownloadCourutine> courutine = Optional.empty();
+	private Object lock = new Object();
+
+	public FeedzillaDownloadService() throws IOException, SQLException {
+		final FeedzillaDownloaderDatabaseSettings databaseSettings = new FeedzillaDownloaderDatabaseSettings(
+				"./config/feedzilla_downloader_production.properties");
+		this.settingsStorage = new FeedzillaDownloaderDatabaseStorage(databaseSettings);
+		this.downloaderLogger = new OrmliteFeedzillaDownloaderLogger(logger, settingsStorage, settingName, getProcessId(), getStartTime());
+		this.settings = settingsStorage.getSettings(settingName);
+		downloaderLogger.log(StatisticType.TRACE, "YahooDownloadService initialized");
+	}
+
+	private FeedzillaDownloaderSettings readSettings() throws SQLException {
+		try {
+			settings = settingsStorage.getSettings(settingName);
+		} catch (SQLException e) {
+			logger.fatal("readSettings() " + e.getMessage());
+		}
+		return settings;
 	}
 
 	public static void main(String[] args) {
@@ -34,19 +69,85 @@ final class FeedzillaDownloadService implements StopableApp {
 
 	@Override
 	public void start() throws Exception {
-		// TODO Auto-generated method stub
+		long start = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+		while (!stopped) {
+			readSettings();
+			downloaderLogger.log(StatisticType.TRACE, "Going to start next download cycle of " + FeedzillaDownloadService.class);
+			download();
+			downloaderLogger.log(StatisticType.TRACE, "Downloading finished");
+			if (stopped) {
+				break;
+			}
+			final long timeDiff = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) - start;
+			final int intervalBetweenExecutionsSeconds = settings.intervalBetweenExecutions();
+			if (timeDiff < intervalBetweenExecutionsSeconds) {
+				synchronized (lock) {
+					final long secondsSleepInterval = (intervalBetweenExecutionsSeconds - timeDiff);
+					final double minutesSleepInterval = (double) secondsSleepInterval / 3600;
+					downloaderLogger.log(StatisticType.TRACE, "Sleep until next cycle: " + (intervalBetweenExecutionsSeconds - timeDiff)
+							+ " seconds (" + minutesSleepInterval + " hours)");
+					lock.wait(1000 * (intervalBetweenExecutionsSeconds - timeDiff));
+				}
+			}
+			start = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+		}
+	}
 
+	private void download() throws SQLException {
+		// try {
+		// final YahooDownloaderSettings s = settings;
+		// final boolean downloadExisted = s.downloadOnlyExisted();
+		// final YahooSettings settings = YahooUtils.createSettings();
+		// final boolean downloadByPattern = s.downloadByPattern();
+		// final String startPattern = s.patternNameFrom();
+		// final String endPattern = s.patternNameTo();
+		// final int stockNameMinLength = s.stockNameFrom();
+		// final int stockNameMaxLength = s.stockNameTo();
+		// final int downloadThreadSize = s.threadAmount();
+		// final YahooDownloadCourutine courutine = new
+		// YahooDownloadCourutine(downloaderLogger, downloadExisted, settings,
+		// downloadByPattern, startPattern, endPattern, stockNameMinLength,
+		// stockNameMaxLength, downloadThreadSize);
+		// synchronized (this) {
+		// this.courutine = Optional.of(courutine);
+		// }
+		// courutine.start();
+		// synchronized (this) {
+		// this.courutine = Optional.empty();
+		// }
+		// } catch (Exception e) {
+		// downloaderLogger.log(StatisticType.ERROR, "download() " +
+		// e.getMessage());
+		// }
 	}
 
 	@Override
 	public void stop() throws Exception {
-		// TODO Auto-generated method stub
-
+		this.stopped = true;
+		synchronized (this) {
+			// if (courutine.isPresent()) {
+			// courutine.get().stop();
+			// }
+		}
+		synchronized (lock) {
+			lock.notifyAll();
+		}
 	}
 
 	@Override
 	public void log(Level logLevel, String message) {
 		logger.error("log(Level, String): " + logLevel.getName() + " " + message);
+	}
+
+	private Integer getProcessId() {
+		final String name = ManagementFactory.getRuntimeMXBean().getName();
+		final String id = name.substring(0, name.indexOf('@'));
+		return Integer.valueOf(id);
+	}
+
+	private Date getStartTime() {
+		final long startTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+		return new Date(startTime);
 	}
 
 }
--- stsc.database/storage/src/main/java/stsc/database/service/schemas/OrmliteFeedzillaDownloaderLogger.java
+++ stsc.database/storage/src/main/java/stsc/database/service/schemas/OrmliteFeedzillaDownloaderLogger.java
@@ -7,18 +7,18 @@ import org.apache.logging.log4j.Logger;
 
 import stsc.common.service.statistics.FeedzillaDownloaderLogger;
 import stsc.common.service.statistics.StatisticType;
-import stsc.database.service.storages.YahooDownloaderDatabaseStorage;
+import stsc.database.service.storages.FeedzillaDownloaderDatabaseStorage;
 
 public class OrmliteFeedzillaDownloaderLogger implements FeedzillaDownloaderLogger {
 
 	private final Logger logger;
-	private final YahooDownloaderDatabaseStorage storage;
+	private final FeedzillaDownloaderDatabaseStorage storage;
 	private final String settingName;
 	private final int processId;
 	private final Date startDate;
 
-	public OrmliteFeedzillaDownloaderLogger(final Logger logger, final YahooDownloaderDatabaseStorage storage, final String settingName,
-			final int processId, final Date startDate) {
+	public OrmliteFeedzillaDownloaderLogger(final Logger logger, final FeedzillaDownloaderDatabaseStorage storage,
+			final String settingName, final int processId, final Date startDate) {
 		this.logger = logger;
 		logger.info("logger initialized. processId: " + processId + ", startDate: " + startDate);
 		this.settingName = settingName;
@@ -29,7 +29,7 @@ public class OrmliteFeedzillaDownloaderLogger implements FeedzillaDownloaderLogg
 
 	@Override
 	public boolean log(StatisticType statisticType, String message) {
-		final OrmliteYahooDownloaderStatistics s = new OrmliteYahooDownloaderStatistics(settingName);
+		final OrmliteFeedzillaDownloaderStatistics s = new OrmliteFeedzillaDownloaderStatistics(settingName);
 		s.setProcessId(processId);
 		s.setStartDate(startDate);
 		s.setStatisticType(statisticType);
