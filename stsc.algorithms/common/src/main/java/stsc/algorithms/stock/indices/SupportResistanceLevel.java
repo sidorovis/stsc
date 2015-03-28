e230282e57eda118b8016778a3f3a6f9902b6188
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-03-29 00:23:34 +0100

    #50 added support and resistance level algorithm implementation prototype details

--- stsc.algorithms/common/src/main/java/stsc/algorithms/stock/indices/SupportResistanceLevel.java
+++ stsc.algorithms/common/src/main/java/stsc/algorithms/stock/indices/SupportResistanceLevel.java
@@ -1,7 +1,12 @@
 package stsc.algorithms.stock.indices;
 
 import java.util.Optional;
+import java.util.PriorityQueue;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import stsc.algorithms.stock.indices.primitive.SmStDev;
+import stsc.algorithms.stock.indices.primitive.Sma;
 import stsc.common.BadSignalException;
 import stsc.common.Day;
 import stsc.common.algorithms.BadAlgorithmException;
@@ -9,20 +14,35 @@ import stsc.common.algorithms.StockAlgorithm;
 import stsc.common.algorithms.StockAlgorithmInit;
 import stsc.common.signals.SerieSignal;
 import stsc.common.signals.SignalsSerie;
+import stsc.signals.DoubleSignal;
+import stsc.signals.series.LimitSignalsSerie;
 
 public class SupportResistanceLevel extends StockAlgorithm {
 
-	private int lastMinN;
+	private final String subExecutionName;
+
+	private final int N;
+	private final int M;
+	
+	private final PriorityQueue<Pair<Integer, Double>> lastMins = new PriorityQueue<>(initialCapacity, comparator)
 
 	public SupportResistanceLevel(StockAlgorithmInit init) throws BadAlgorithmException {
 		super(init);
-		this.lastMinN = init.getSettings().getIntegerSetting("lastMinN", 8).getValue();
+		if (init.getSettings().getSubExecutions().size() > 0) {
+			throw new BadAlgorithmException("sub executions settings for " + SupportResistanceLevel.class.toString()
+					+ " should have at least one algorithm");
+		}
+		this.subExecutionName = init.getSettings().getSubExecutions().get(0);
+		this.N = init.getSettings().getIntegerSetting("N", 8).getValue();
+		this.M = init.getSettings().getIntegerSetting("M", 66).getValue();
 
+		// SmStDev
 	}
 
 	@Override
 	public Optional<SignalsSerie<SerieSignal>> registerSignalsClass(StockAlgorithmInit initialize) throws BadAlgorithmException {
-		return Optional.empty();
+		final int size = initialize.getSettings().getIntegerSetting("size", 2).getValue().intValue();
+		return Optional.of(new LimitSignalsSerie<>(DoubleSignal.class, size));
 	}
 
 	@Override
