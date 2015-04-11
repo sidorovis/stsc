83534f6419742c94ebdace993e1c7c6388fb0bf3
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-04-11 13:33:34 +0200

    #141 separated stsc.distributed.hadoop

--- stsc.distributed.hadoop/src/main/java/stsc/distributed/hadoop/grid/HadoopStarter.java
+++
@@ -1,9 +0,0 @@
-package stsc.distributed.hadoop.grid;
-
-import java.util.List;
-
-import stsc.general.strategy.TradingStrategy;
-
-public interface HadoopStarter {
-	public List<TradingStrategy> searchOnHadoop() throws Exception;
-}
