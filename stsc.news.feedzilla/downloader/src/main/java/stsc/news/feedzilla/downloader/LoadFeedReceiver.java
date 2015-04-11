933cc808ab1f1987d8f7473b3fed1833682a3794
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-04-11 19:40:55 +0200

    #141 separated stsc.news.feedzilla

--- stsc.news.feedzilla/downloader/src/main/java/stsc/news/feedzilla/downloader/LoadFeedReceiver.java
+++
@@ -1,9 +0,0 @@
-package stsc.news.feedzilla.downloader;
-
-import graef.feedzillajava.Article;
-import graef.feedzillajava.Category;
-import graef.feedzillajava.Subcategory;
-
-public interface LoadFeedReceiver {
-	public void newArticle(Category category, Subcategory subcategory, Article article);
-}
--- stsc.news.feedzilla/storage/src/main/java/stsc/news/feedzilla/FeedzillaFileStorageReceiver.java
+++
@@ -1,9 +0,0 @@
-package stsc.news.feedzilla;
-
-import stsc.news.feedzilla.file.schema.FeedzillaFileArticle;
-
-public interface FeedzillaFileStorageReceiver {
-
-	public boolean addArticle(FeedzillaFileArticle article);
-
-}
