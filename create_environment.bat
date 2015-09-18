64f4109e9b5e76a92c1aca70bb55bab3828ff698
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-09-18 23:06:09 +0200

    added create_environment.bat script

---
+++ create_environment.bat
@@ -0,0 +1,33 @@
+﻿@echo off
+
+IF EXIST ./source GOTO EXIT
+echo "Creating Sources..."
+mkdir source
+cd source
+call :cloneAndCreateProject stsc.common
+call :cloneAndCreateProject stsc.algorithms
+call :cloneAndCreateProject stsc.storage
+call :cloneAndCreateProject stsc.signals
+call :cloneAndCreateProject stsc.stocks.yahoo
+call :cloneAndCreateProject stsc.stocks.indexes
+call :cloneAndCreateProject stsc.database
+call :cloneAndCreateProject stsc.general
+call :cloneAndCreateProject stsc.performance
+call :cloneAndCreateProject stsc.integration.tests
+call :cloneAndCreateProject stsc.fundamental.analysis
+call :cloneAndCreateProject stsc.news.feedzilla
+call :cloneAndCreateProject stsc.distributed.hadoop
+call :cloneAndCreateProject stsc.frontend.zozka
+call :cloneAndCreateProject stsc.as.service.feedzilla.downloader
+call :cloneAndCreateProject stsc.as.service.yahoo.downloader
+
+GOTO :EXIT
+
+:cloneAndCreateProject
+    echo Cloning and Creating %~1 Project
+    git clone https://github.com/sidorovis/%~1.git
+    cd %~1
+    mvn eclipse:eclipse -DdownloadSources=true -DdownloadJavadocs=true
+goto:eof
+
+:EXIT
